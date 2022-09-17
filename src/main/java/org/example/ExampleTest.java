c9ddafabe66d76ad89d1a51a2bd555fbe855ce08
Василий Мараховский <tombolone@yandex.ru>
2022-09-17 18:22:17 +0400

    fix_logic_use_random

--- src/main/java/org/example/ExampleTest.java
+++ src/main/java/org/example/ExampleTest.java
@@ -2,12 +2,14 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
     @Test
     void exampleTest0(){
-        Assertions.assertTrue(2>1);}
+        Assertions.assertTrue(new Random().nextInt() >1);}
 
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(3>2);}
+        Assertions.assertTrue(new Random().nextInt() >2);}
 }
